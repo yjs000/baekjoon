39dfd5dfad34eeb6fad635164b076688f85987ee
jsyoon <jsyoon@neighbor21.co.kr>
2022-10-06 09:19:24 +0900

    ✨ 2839 완료

--- greedy/2839.js
+++ greedy/2839.js
@@ -4,34 +4,39 @@
 // Greedy [x]
 // 5 만 사용하는 것이 가장 좋음. 5로 나누어떨어지지 않는다면 3을 사용. 
 // 나누어떨어질때까지 5는 하나줄이고, 3은 하나늘림
-// 이걸 어ㅓㄸㅎ게 하지..
+// - 재귀
+// - while
 
-// 수학적
-// 표를 만들어서 규칙을 찾아봄
+// 수학적인 방법
 
-function countBag(N) {
+let readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on("line", function(line){
+    input = Number(line);
+    rl.close();
+}).on("close", function(){
+    let res = countBag(input);
+    console.log(res)
+    process.exit();
+})
+
+function countBag(input) {
     let res = 0;
     function count(N){
         if(N < 0){
             return -1;
         } else if(N % 5 == 0){
-            // console.log("n%5", N, res)
             return res += parseInt(N / 5);
         } else {
-            // console.log("-3", N, res)
             N -= 3;
             res = res + 1;
         } 
         return count(N);
     }
-    return count(N);
+    return count(input);
 }
-
-function print(){
-    const arr = [18,4,6,9,11];
-    arr.forEach( val => {
-        console.log(countBag(val));
-    })        
-}
-
-print();
