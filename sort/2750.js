1bcfde2f962257b376f1ac1a395b2322fdb57616
yjs000 <js91113@gmail.com>
2022-10-17 08:23:25 +0900

    계정 바꿈

---
+++ sort/2750.js
@@ -0,0 +1,17 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./in/2750";
+let input = require("fs").readFileSync(filePath).toString().split("\n");
+
+console.log(solution(input));
+
+//틀렷다고..
+function solution(input) {
+    const val = input.map((str) => Number(str));
+    const set = new Set();
+    const res = [];
+    val.forEach((item) => set.add(item));
+    for (const item of set) {
+        res.push(item);
+    }
+
+    return res.sort((a, b) => a - b);
+}
