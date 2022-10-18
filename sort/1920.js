016e0e7e449866347fd2cb8484ab559c8d5c2435
yjs000 <jisu91113@gmail.com>
2022-10-18 21:59:27 +0900

    🚧 1920 binarySearch구현중

--- sort/1920.js
+++ sort/1920.js
@@ -13,12 +13,25 @@ function solution(input) {
         .map((val) => Number(val))
         .sort((a, b) => a - b);
     const target = last.split(" ").map((val) => Number(val));
-    target.forEach((t) => binarySearch(arr, t) ? res.push(1) : 0);
+    target.forEach((t) => (binarySearch(arr, t) ? res.push(1) : res.push(0)));
     return res;
 }
 
 //구현
 function binarySearch(arr, target) {
-    const idx = parseInt(arr.length / 2);
-    if(target < arr[idx]){}
+    console.log("binarySearch")
+    let res = false
+    let left = 0;
+    let right = arr.length;
+    const idx = parseInt((left + right) / 2);
+    while(left < idx && idx < right) {
+        console.log(left, right, idx) //0, 5, 2 무한루프
+        if (target == arr[idx]) {
+            res = true;
+        } else if (target > arr[idx]) {
+            left = idx;
+        } else if (target < arr[idx]) {
+            right = idx;
+        }       
+    }
 }
